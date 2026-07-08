512f84e40f1965c57a8c77d11b86b981b23da3df
ortegasaxo-spec <ortegasaxo@gmail.com>
2026-07-08 13:19:28 +0200

    3.5

--- credits.js
+++ credits.js
@@ -1,4 +1,8 @@
 (function(global){
+
+  const gameOverImg = new Image();
+gameOverImg.src = "assets/gameover.png";
+
   function getBuildText(){
     return 'D' + upgradeLevels.damage + ' / Cad ' + upgradeLevels.rate + ' / Vel ' + upgradeLevels.speed + ' / Vida ' + upgradeLevels.hp + ' / Disp ' + upgradeLevels.bullets + ' / Dragon ' + dragonballCount + ' / Vidas ' + extraLives + ' / Tech ' + Object.entries(superTechLevels).map(([k,v]) => k + ':' + v).join(',');
   }
@@ -35,7 +39,62 @@
 } 
     ctx.fillStyle='white';
     ctx.font='30px Arial';
-    ctx.fillText('GAME OVER', canvas.width / 2 - 100, 80);
+    // Dibujar imágenes Game Over
+
+if (gameOverImg.complete) {
+
+    const imgW = 220;
+    const imgH = 220;
+
+    // izquierda
+    ctx.drawImage(
+        gameOverImg,
+        canvas.width/2 - 805,
+        250,
+        imgW,
+        imgH
+    );
+
+    // derecha espejada
+    ctx.save();
+
+    ctx.translate(canvas.width/2 + 390 + imgW,0);
+
+    ctx.scale(-1,1);
+
+    ctx.drawImage(
+        gameOverImg,
+        - 105,
+        250,
+        imgW,
+        imgH
+    );
+
+    ctx.restore();
+
+    ctx.fillStyle="white";
+    ctx.font="24px Arial";
+
+    ctx.fillText(
+        "GAME OVER",
+        canvas.width/2-805,
+        700
+    );
+
+    ctx.fillText(
+        "GAME OVER",
+        canvas.width/2+575,
+        700
+    );
+}
+    
+    ctx.font='42px Arial';
+
+ctx.fillText(
+    'RANKING',
+    canvas.width/2-90,
+    70
+);
     ctx.fillStyle = "#4FC3FF";
 ctx.font = "28px Arial";
     ctx.font = '24px Arial';
